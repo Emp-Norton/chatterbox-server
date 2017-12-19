c02f01b1600f70895d1579655a429d334955cdf4
Emp-Norton <jamesjelenko@gmail.com>
2017-12-19 13:13:48 -0800

    appears to be passing all test + functioning as expected."

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,71 +1,56 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-// storage = {
-//   hrsf87: {
-//    lobby: {}, main: {}, cats: {}
-//   }, 
-//   hrsf86: {
-//     main: {}, dinotalk: {}  
-//   }
-// };
-
-var storage = {
-  classes: {
-    hrsf87: {
-      lobby: []
-    }
-  }
-};
-
-
-var requestHandler = function(request, response) {
-
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  var statusCode = 200;
-  //var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = 'text/plain';
-  response.writeHead(statusCode, headers);
-
-  if (request.method === 'POST') {
-    var details = {};
-    request.on('data', function(data) {
-      var text = JSON.parse(data).text;
-      text = JSON.stringify(text);
-      var username = JSON.parse(data).username;
-      var room = JSON.parse(data).room;
-      storage.classes.hrsf87.lobby.push({'text': text, 'username': username, 'room': room});
-    }); 
-    response.end(JSON.stringify(details));
-  } else {
-    // check first term of URL against storage 
-      // if valid, return all messages in that class
-      // else check all message of all classes for that username, collect and return.
-    var term = request.url.split('/');
-    console.log(term[1]);
-    response.end(JSON.stringify(storage.classes[term[1]]));  
-  }
-    
-  response.end('done');
-}; 
-
-
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
+var storage = {
+  classes: {
+  }
+};
+
+
+var requestHandler = function(request, response) {
+  var allMessages = {results: []};
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  var klass = request.url.split('/')[1];
+  var room = request.url.split('/')[2];
+  
+  headers['Content-Type'] = 'text/plain';
+  response.writeHead(statusCode, headers);
+
+  if (!storage.hasOwnProperty(klass)) {
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end('invalid');
+  }
+
+  if (request.method === 'POST') {
+    request.on('data', function(data) {      
+      var text = JSON.parse(data).message;
+      var username = JSON.parse(data).username;
+      var details = {'message': text, 'username': username, 'room': room};
+
+      if (storage[klass][room]) { 
+        storage[klass][room].push(details);  
+      } else {
+        storage[klass][room] = [{'message': text, 'username': username, 'room': room}];
+      } 
+      allMessages.results = [details];
+      statusCode = 201;
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(allMessages));
+    }); 
+  } else { // GET REQUESTS
+    allMessages.results = storage.classes[room] || [];
+    response.end(JSON.stringify(allMessages));  
+  }
+};
+
+
+
 
 module.exports.requestHandler = requestHandler;
